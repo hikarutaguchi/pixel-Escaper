ac4f39f947c81b039ee938a5fc867268134dcb57
ooojun <ozaki_jun_123@icloud.com>
2021-10-22 12:10:34 +0900

    Transition追加

---
+++ Assets/Scripts/Scenerelated/Transition.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Transition : MonoBehaviour
+{
+    [SerializeField]
+    private Material _transitionIn;
+
+    void Start()
+    {
+        StartCoroutine(BeginTransition());
+    }
+
+    IEnumerator BeginTransition()
+    {
+        yield return Animate(_transitionIn, 1);
+    }
+
+    /// <summary>
+    /// time秒かけてトランジションを行う
+    /// </summary>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    IEnumerator Animate(Material material, float time)
+    {
+        GetComponent<Image>().material = material;
+        float current = 0;
+        while (current < time)
+        {
+            material.SetFloat("_Alpha", current / time);
+            yield return new WaitForEndOfFrame();
+            current += Time.deltaTime;
+        }
+        material.SetFloat("_Alpha", 1);
+    }
+}
