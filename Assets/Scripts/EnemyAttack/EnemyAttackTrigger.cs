d3ecd8a2b2bc8620272e27a7d5fde50fde2190a0
KURODAHYUN <conanyuto@icloud.com>
2021-12-01 15:22:37 +0900

    のだ

--- Assets/Scripts/EnemyAttack/EnemyAttackTrigger.cs
+++ Assets/Scripts/EnemyAttack/EnemyAttackTrigger.cs
@@ -15,11 +15,30 @@ public class EnemyAttackTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if (Music.IsJustChangedBeat())
+        if (Music.IsJustChangedAt(1, 0, 1))
         {
             manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
             manager.CreateDagger();
         }
+        if (Music.IsJustChangedAt(1, 2, 0))
+        {
+            manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
+            manager.CreateDagger();
+        }
+        if (Music.IsJustChangedAt(1, 2, 3))
+        {
+            manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
+            manager.CreateDagger();
+        }
+        //if(Music.IsJustChangedBar())
+        //{
+        //    manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
+        //    manager.CreateDagger();
+        //}
+        //if (Music.IsJustChangedBeat())
+        //{
+        //    manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
+        //    manager.CreateDagger();
+        //}
     }
 }
