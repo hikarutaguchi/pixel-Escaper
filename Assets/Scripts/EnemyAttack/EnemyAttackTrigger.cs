d3bbd2cc12b744ab876001bd966eec8972df5d2b
KURODAHYUN <conanyuto@icloud.com>
2021-12-01 11:01:03 +0900

    リズム天国ゴールド

--- Assets/Scripts/EnemyAttack/EnemyAttackTrigger.cs
+++ Assets/Scripts/EnemyAttack/EnemyAttackTrigger.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class EnemyAttackTrigger : MonoBehaviour
@@ -8,13 +9,14 @@ public class EnemyAttackTrigger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Music.Play("Square");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        
+        if (Music.IsJustChangedBeat())
         {
             manager = GameObject.Find("GameObject").GetComponent<EnemyAttackManager>();
             manager.CreateDagger();
