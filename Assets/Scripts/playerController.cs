509a575ca7ce52e2a2a7ed0cb39c87588d3f64e7
ooojun <ozaki_jun_123@icloud.com>
2021-12-09 10:57:05 +0900

    add dino 操作制御

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class playerController : MonoBehaviour
 {
-    Vector3 MOVEX = new Vector3(1.5f, 0, 0); // x軸方向に１マス移動するときの距離
-    Vector3 MOVEY = new Vector3(0, 1.5f, 0); // y軸方向に１マス移動するときの距離
+	Vector3 MOVEX = new Vector3(1.0f, 0, 0); // x軸方向に１マス移動するときの距離
+	Vector3 MOVEY = new Vector3(0, 1.0f, 0); // y軸方向に１マス移動するときの距離
 
-    float step = 10f;     // 移動速度
-    Vector3 target;      // 入力受付時、移動後の位置を算出して保存 
-    Vector3 prevPos;     // 何らかの理由で移動できなかった場合、元の位置に戻すため移動前の位置を保存
+	/// <summary>
+	/// タイルマップデータ
+	/// </summary>
+	[SerializeField] private GameObject tileMap;
+
+	float step = 10f;     // 移動速度
+	Vector3 target;      // 入力受付時、移動後の位置を算出して保存 
+	Vector3 prevPos;     // 何らかの理由で移動できなかった場合、元の位置に戻すため移動前の位置を保存
 	bool upushFlag = false;
 	bool dpushFlag = false;
 	bool lpushFlag = false;
@@ -17,17 +23,24 @@ public class playerController : MonoBehaviour
 
 	Animator animator;   // アニメーション
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = transform.position;
-        animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
+	private void Awake()
 	{
-        //① 移動中かどうかの判定。移動中でなければ入力を受付
+		target = transform.position;
+		var tilemap = tileMap.GetComponent<Tilemap>();
+		var position = new Vector3Int(-4, 0, 0);
+		target = new Vector3(tilemap.GetCellCenterLocal(position).x, tilemap.GetCellCenterLocal(position).y, 0);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//① 移動中かどうかの判定。移動中でなければ入力を受付
 		if (transform.position == target)
 		{
 			SetTargetPosition();
@@ -47,12 +60,12 @@ public class playerController : MonoBehaviour
 			{
 				rpushFlag = true;    // 押し状態にする
 				target = transform.position + MOVEX;
-				//SetAnimationParam(1);			
+				SetAnimationParam(1);
 			}
 		}
 		else
 		{
-			rpushFlag = false;    
+			rpushFlag = false;
 		}
 
 		if (Input.GetKey(KeyCode.LeftArrow))
@@ -61,7 +74,7 @@ public class playerController : MonoBehaviour
 			{
 				lpushFlag = true;    // 押し状態にする
 				target = transform.position - MOVEX;
-				//SetAnimationParam(2);
+				SetAnimationParam(2);
 			}
 		}
 		else
@@ -75,7 +88,7 @@ public class playerController : MonoBehaviour
 			{
 				upushFlag = true;    // 押し状態にする
 				target = transform.position + MOVEY;
-				//SetAnimationParam(3);
+				SetAnimationParam(3);
 			}
 		}
 		else
@@ -89,19 +102,24 @@ public class playerController : MonoBehaviour
 			{
 				dpushFlag = true;    // 押し状態にする
 				target = transform.position - MOVEY;
-				//SetAnimationParam(0);
+				SetAnimationParam(0);
 			}
 		}
 		else
 		{
 			dpushFlag = false;
 		}
+		if(target.x > -1 || target.x < -6 || target.y > 3 || target.y < -2)
+        {
+			target = prevPos;
+		}
 	}
 
 	// WalkParam  0;下移動　1;右移動　2:左移動　3:上移動
 	void SetAnimationParam(int param)
 	{
-		animator.SetInteger("WalkParam", param);
+		animator.SetTrigger("WorkFlag");
+		//animator.SetInteger("WalkParam", param);
 	}
 
 	// ③ 目的地へ移動する
